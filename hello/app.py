359c9d69cbdb491503b305fb8f0306d6e2e4e0fa
Dave Forgac <dave.forgac@complion.com>
2019-08-12 00:18:13 -0400

    get version from env

--- hello/app.py
+++ hello/app.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask
 
 app = Flask(__name__)
@@ -6,3 +8,9 @@ app = Flask(__name__)
 @app.route("/")
 def hello_pipelines():
     return "Hello, pipelines!"
+
+
+@app.route("/version")
+def version():
+    version = os.environ.get("HELLO_PIPELINES_VERSION")
+    return f"Version: {version}"
